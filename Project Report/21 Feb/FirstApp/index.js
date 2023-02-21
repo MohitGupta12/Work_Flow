196b0f27bbc353752065db4c648d0cc4fa251809
Mohit <mohit2412gupta@gmail.com>
2023-02-21 23:55:07 +0530

    Project Report Update

--- Project Report/21 Feb/FirstApp/index.js
+++ Project Report/21 Feb/FirstApp/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
 // console.dir(app);
-app.use((req, res) => {
-    console.log("We got a request!!")
-    res.send("We got your Request, Sargent :0")
-}
-)
+// app.use((req, res) => {
+//     console.log("We got a request!!")
+//     res.send("We got your Request, Sargent :0")
+// }
+// )
+
+app.get('/', (req, res) => {
+    res.send('<h1>We are on Home Page</h1>')
+})
+
+app.get('/locker', (req, res) => {
+    res.send('<h1>We are in locker</h1>')
+})
+
+app.get('/Community', (req, res) => {
+    res.send('<h1>We are on Community Page </h1>')
+})
+app.get('*', (req, res) => {
+    res.send('<h1>We don\'t belong <b>Here</b></h1>')
+})
+
+
 app.listen(3000, () => {
     console.log("We are listening on port 3000!");
 })
