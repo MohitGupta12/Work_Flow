9ffe59b2f66423edeed748cdf040c080d538ac71
Mohit <mohit2412gupta@gmail.com>
2023-02-22 00:23:07 +0530

    Project Report Update 6

--- Project Report/21 Feb/FirstApp/index.js
+++ Project Report/21 Feb/FirstApp/index.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.send('<h1>We are on Home Page</h1>')
 })
 
+app.get('/locker/:docs', (req, res) => {
+    const { docs } = req.params
+    res.send(`<h1>We are looking for ${docs} in locker</h1>`)
+})
 app.get('/locker', (req, res) => {
     res.send('<h1>We are in locker</h1>')
 })
@@ -18,6 +22,15 @@ app.get('/locker', (req, res) => {
 app.get('/Community', (req, res) => {
     res.send('<h1>We are on Community Page </h1>')
 })
+
+app.get('/Community/search', (req, res) => {
+    const { q, color } = req.query;
+    if (!q) {
+        res.send(6969);
+    }
+    res.send(`<h1>We are on Community Page searching for ${q} and color is ${color} </h1>`)
+})
+
 app.get('*', (req, res) => {
     res.send('<h1>We don\'t belong <b>Here</b></h1>')
 })
